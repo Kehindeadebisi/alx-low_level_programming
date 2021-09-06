a4d55b04fe68e80ecdbf734bade52fc68ae6d8a2
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-06 12:32:24 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *Return: 0
+ */
+
+void _puts(char *str);
+{
+	puts("I do not fear computers. I fear the lack of them - Isaac Asimov");
+	return (0);
+}

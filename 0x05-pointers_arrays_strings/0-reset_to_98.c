b5bbf1c7544963d26afb08080d9fa4072499d192
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-06 06:00:05 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <holberton.h>
 
 /**
  * reset_to_98 - takes a pointer to an int
@@ -9,10 +9,5 @@
 
 reset_to_98(int *n)
 {
-	int i;
-	int *n;
-
-	*n = &i;
 	*n = 98;
-
 }

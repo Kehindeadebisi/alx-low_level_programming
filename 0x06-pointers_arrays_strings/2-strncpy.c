d947e57168107c5bec2706dbdf75119ae58f50e0
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-09 07:15:36 -0700

    copies strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,11 +17,13 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		j++;
 	}
+
 	while (i < n && src[i])
 	{
 		dest[i] = src[i];
-		i++
-			}
+		i++;
+	}
+
 	while (i < n)
 	{
 		dest[i] = '\0';

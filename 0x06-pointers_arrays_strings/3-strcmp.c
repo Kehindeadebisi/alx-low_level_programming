2053d0443ef86e3315124d7f3bb698844423bd89
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-09 09:01:36 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	if (s1[i] == s2[j]);
+	while (*s1 == *s2)
 	{
-		return (0);
-
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
 	}
-	else
-	{
-		return (s1[i] - s2[j]);
-	}
+	return (*s1 - *s2);
 }

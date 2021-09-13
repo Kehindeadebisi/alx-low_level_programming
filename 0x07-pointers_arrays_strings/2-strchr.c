980c967d8661491805ea438e9ab4181866214329
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-13 10:33:39 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,21 +9,10 @@
   */
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
-
-	while (s[a])
+	while (*s++)
 	{
-		a++;
-	}
-
-	for (b = 0; b <= a; b++)
-	{
-		if (c == s[b])
-		{
-			s += b;
+		if (*s == c)
 			return (s);
-		}
 	}
-
-	return ('\0');
+	return (NULL);
 }

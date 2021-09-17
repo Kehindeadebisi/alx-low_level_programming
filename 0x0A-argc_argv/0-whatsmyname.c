2e2a40364623da3b8880a5fae0a462ab649919a3
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-17 05:12:04 -0700

    fixed errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ *main - main function
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
-	_putchar (%s\n, argv[0]);
+	_putchar ("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,6 @@
 #ifndif MAIN_H
 #define MAIN_H
 
-int _putchar(char *c);
-int main(int argc, char *argv[]);
+int _putchar(char c);
 
 #endif

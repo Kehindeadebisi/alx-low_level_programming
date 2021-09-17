4bce243a21d6165b250b0ed489d010e2e63b8018
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-17 08:17:21 -0700

    added tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - multiplies numbers
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	mul = 1;
+
+	if (argc == 3)
+	{
+		for (i = 1; 1 < argc; i++)
+		{
+                        mul *= atoi(argv[i]);
+		}
+
+		printf("%d\n", mul);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}

f0cbf7ce0705239394438c3936a4cbfff819a94f
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-09-17 11:38:33 -0700

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,21 +1,55 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
- * get_change - Prints the minimum number of coins for change
- * @n: Number of cents to get change
- *
- * Return: 0 - Success
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
-void get_change(int n)
+int main(int argc, char *argv[])
 {
-        int left;
-        int num;
-        int i;
-        int count;
-        int coins[] = {25, 10, 5, 2, 1};
+	int value, c;
 
-        left = n;
-        count = 0;
-
-        if (n > 0)
-        {
-                for (i = 0; i < 5; i++)
+	c = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
+	return (0);
+}

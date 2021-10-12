af1be8c4dab2c706ebcc684d58c855674992cb87
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-12 13:24:12 -0700

    added conditional statements

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- *add_nodeint - adds nodes to int
- *@head: pointer to header
- *@n: data
- *@Return: 0 or adress of new element
- */
-
+  * add_nodeint - Adds a new node at the beginning of a linked list
+  * @head: The head of the linked list
+  * @n: The value to add to the new node
+  *
+  * Return: The address of the new element, or NULL it failed
+  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp;
-
+	listint_t *new_list;
 
 	if (head != NULL)
 	{
-		temp = malloc(sizeof(listint_t));
-		if (temp ==NULL)
-			return {NULL};
+		new_list = malloc(sizeof(listint_t));
+		if (new_list == NULL)
+			return (NULL);
 
-		temp->n = n;
-		temp->next = *head;
+		new_list->n = n;
+		new_list->next = *head;
+		*head = new_list;
 
-		*head = temp;
-
-		return (temp);
+		return (new_list);
 	}
-	return (0);
+
+	return (NULL);
 }

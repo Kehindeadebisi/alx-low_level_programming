41980f47c68cbfff21669b067a44a0ff7df3fb3a
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-12 08:54:29 -0700

    fixed errors

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_HEADER_
+#define LISTS_HEADER_
 
 /**
  * struct listint_s - singly linked list
@@ -11,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
@@ -27,6 +27,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
 
 #endif

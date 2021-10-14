c7d950630766794803e0d395fa0207e77818131d
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-14 11:01:44 -0700

    added tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
   * binary_to_uint - Converts a binary number to an unsigned int
@@ -8,20 +8,40 @@
   *
   * Return: The positive number converted from a binary
   */
-
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
+	unsigned int len = 0, count = 0, sum = 0;
 
-  if (b == NULL)
-    return 0;
+	if (b == NULL)
+		return (0);
 
-  while (b[i] == '0' || b[i] == '1')
-  {  // Found another digit.
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
+	len = _strlen(b);
+	while (len--)
+	{
+		if (b[len] != 48 && b[len] != 49)
+			return (0);
+
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		count++;
+	}
+
+	return (sum);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }

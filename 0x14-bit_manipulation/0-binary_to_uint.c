7cc7d5b38140ddb26fdc93cbe04fad5c261eb69c
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-14 10:09:17 -0700

    added REAMME and tak

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * binary_to_uint - Converts a binary number to an unsigned int
+  * @b: The binary string to converts
+  *
+  * Return: The positive number converted from a binary
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+  unsigned int val = 0;
+  int i = 0;
+
+  if (b == NULL)
+    return 0;
+
+  while (b[i] == '0' || b[i] == '1')
+  {  // Found another digit.
+    val <<= 1;
+    val += b[i]-'0';
+    i++;
+  }
+  return val;
+}

522a66552ef2be203310965581a563d1fd0e9497
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-10-18 13:33:04 -0700

    added files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ *read_textfile - reads textfile
+ *@filename: file
+ *@letters: number of letters to be read and printed
+ *
+ *Return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	void *buf;
+	size_t n;
+
+
+	fd = open("filename", O_RDONLY);
+
+	if (fd == -1 || filename == NULL)
+		return (0);
+
+	read(fd, buf, n);
+
+	while (n != '\0')
+	{
+		n++;
+		_putchar(n);
+	}
+
+	return (letters);
+}
